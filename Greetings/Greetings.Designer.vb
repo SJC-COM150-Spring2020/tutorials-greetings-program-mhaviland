505d3318bf773ba7e686d4ac76d1c113f9ae9b07
mhaviland <60245207+mhaviland@users.noreply.github.com>
2020-02-06 20:26:07 -0500

    printed greetings listbox and added picture box
    used listbox to print value of txtName to lstOut. Added clear and exit buttons. Added picGreeetings

---
+++ Greetings/Greetings.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Greetings
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Greetings))
+        Me.btnClick = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lstOut = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.picGreeting = New System.Windows.Forms.PictureBox()
+        CType(Me.picGreeting, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnClick
+        '
+        Me.btnClick.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.btnClick.Font = New System.Drawing.Font("Lucida Sans", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClick.Location = New System.Drawing.Point(37, 290)
+        Me.btnClick.Name = "btnClick"
+        Me.btnClick.Size = New System.Drawing.Size(203, 52)
+        Me.btnClick.TabIndex = 0
+        Me.btnClick.Text = "Click Me"
+        Me.btnClick.UseVisualStyleBackColor = False
+        '
+        'txtName
+        '
+        Me.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtName.Font = New System.Drawing.Font("Lucida Sans", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(37, 233)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(198, 32)
+        Me.txtName.TabIndex = 1
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Lucida Sans", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(31, 25)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(281, 33)
+        Me.lblHeading.TabIndex = 2
+        Me.lblHeading.Text = "Greetings Program"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Lucida Sans", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(43, 196)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(192, 23)
+        Me.lblName.TabIndex = 3
+        Me.lblName.Text = "Enter your name"
+        '
+        'lstOut
+        '
+        Me.lstOut.Font = New System.Drawing.Font("Lucida Sans", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstOut.FormattingEnabled = True
+        Me.lstOut.ItemHeight = 24
+        Me.lstOut.Location = New System.Drawing.Point(37, 361)
+        Me.lstOut.Name = "lstOut"
+        Me.lstOut.Size = New System.Drawing.Size(331, 76)
+        Me.lstOut.TabIndex = 4
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Lucida Sans", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(81, 469)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 41)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Lucida Sans", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(237, 469)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 41)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'picGreeting
+        '
+        Me.picGreeting.Image = CType(resources.GetObject("picGreeting.Image"), System.Drawing.Image)
+        Me.picGreeting.Location = New System.Drawing.Point(503, 104)
+        Me.picGreeting.Name = "picGreeting"
+        Me.picGreeting.Size = New System.Drawing.Size(431, 355)
+        Me.picGreeting.TabIndex = 7
+        Me.picGreeting.TabStop = False
+        '
+        'Greetings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Linen
+        Me.ClientSize = New System.Drawing.Size(1019, 598)
+        Me.Controls.Add(Me.picGreeting)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstOut)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnClick)
+        Me.Name = "Greetings"
+        Me.Text = "Greetings"
+        CType(Me.picGreeting, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnClick As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents lstOut As ListBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents picGreeting As PictureBox
+End Class
---
+++ Greetings/Greetings.vb
@@ -0,0 +1,26 @@
+﻿Public Class Greetings
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+
+        'Print Greetings to listbox
+        lstOut.Items.Add("Good Evening " & txtName.Text)
+        lstOut.Items.Add("How are you?")
+
+
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear txtName value
+        txtName.Clear()
+        lstOut.Items.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'when user exits the program
+        Beep()
+        Application.Exit()
+
+    End Sub
+End Class
+
